a15c49ee8104fe118f66567a2b3f567a1ea4f1be
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-06-19 14:36:39 -0700

    Add proper port of code as-is

--- demos/u8g2_testing/src/main.c
+++ demos/u8g2_testing/src/main.c
@@ -1,8 +1,88 @@
 #include "clib/u8g2.h"
 #include <util/delay.h>
-// #include "lcdbase.h"
+#include "common.h"
 
 #define P_CPU_NS (1000000000UL / F_CPU)
+
+uint8_t u8x8_byte_avr_hw_spi (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
+  uint8_t *data;
+
+  switch (msg) {
+    case U8X8_MSG_BYTE_INIT:
+      srxePinMode(SPI_SCK, OUTPUT);
+      srxePinMode(SPI_MOSI, OUTPUT);
+
+      SPCR = (_BV (SPE) | _BV (MSTR));
+
+      switch (u8x8->display_info->spi_mode) {
+        case 0:
+          break;
+        case 1:
+          SPCR |= _BV (CPHA);
+          break;
+        case 2:
+          SPCR |= _BV (CPOL);
+          break;
+        case 3:
+          SPCR |= _BV (CPOL);
+          SPCR |= _BV (CPHA);
+          break;
+      };
+
+      switch (F_CPU / u8x8->display_info->sck_clock_hz) {
+        case 2:
+          SPSR |= _BV (SPI2X);
+          break;
+        case 4:
+          break;
+        case 8:
+          SPSR |= _BV (SPI2X);
+          SPCR |= _BV (SPR0);
+          break;
+        case 16:
+          SPCR |= _BV (SPR0);
+          break;
+        case 32:
+          SPSR |= _BV (SPI2X);
+          SPCR |= _BV (SPR1);
+          break;
+        case 64:
+          SPCR |= _BV (SPR1);
+          break;
+        case 128:
+          SPCR |= _BV (SPR1);
+          SPCR |= _BV (SPR0);
+          break;
+      }
+
+      srxeDigitalWrite(SPI_CS, u8x8->display_info->chip_disable_level);
+      break;
+    case U8X8_MSG_BYTE_SET_DC:
+      srxePinMode(LCD_DC, arg_int);
+      break;
+    case U8X8_MSG_BYTE_START_TRANSFER:
+      srxeDigitalWrite(SPI_CS, u8x8->display_info->chip_enable_level);
+      u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->post_chip_enable_wait_ns, NULL);
+      break;
+    case U8X8_MSG_BYTE_SEND:
+      data = (uint8_t *) arg_ptr;
+      while (arg_int > 0) {
+          SPDR = (uint8_t) * data;
+          while (!(SPSR & _BV (SPIF)));
+          data++;
+          arg_int--;
+      }
+      break;
+    case U8X8_MSG_BYTE_END_TRANSFER:
+      u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->pre_chip_disable_wait_ns, NULL);
+      srxeDigitalWrite(SPI_CS, u8x8->display_info->chip_disable_level);
+      break;
+    default:
+      return 0;
+  }
+  
+  return 1;
+}
 
 uint8_t u8x8_avr_delay (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
 	uint8_t cycles;
@@ -41,27 +121,6 @@ uint8_t u8x8_avr_delay (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_p
 	return 1;
 }
 
-// #define SPI_CS		(SRXE_PORTB | PIN0)
-// #define SPI_SCK		(SRXE_PORTB | PIN1)
-// #define SPI_MOSI	(SRXE_PORTB | PIN2)
-// #define SPI_MISO	(SRXE_PORTB | PIN3)
-
-// #define LCD_CS	 	(SRXE_PORTE | PIN7)
-// #define LCD_DC	 	(SRXE_PORTD | PIN6)
-// #define LCD_RESET	(SRXE_PORTG | PIN2)
-
-#define CS_DDR DDRE
-#define CS_PORT PORTE
-#define CS_BIT PIN7
-
-#define DC_DDR DDRD
-#define DC_PORT PORTD
-#define DC_BIT PIN6
-
-#define RESET_DDR DDRG
-#define RESET_PORT PORTG
-#define RESET_BIT PIN2
-
 // https://github.com/olikraus/u8g2/blob/master/sys/avr/avr-libc/atmega328p/main.c
 uint8_t
 u8x8_gpio_and_delay (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
@@ -73,30 +132,21 @@ u8x8_gpio_and_delay (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
     // called once during init phase of u8g2/u8x8
     // can be used to setup pins
     case U8X8_MSG_GPIO_AND_DELAY_INIT:
-      CS_DDR |= _BV(CS_BIT);
-      DC_DDR |= _BV(DC_BIT);
-      RESET_DDR |= _BV(RESET_BIT);
+      srxePinMode(LCD_CS, OUTPUT);
+      srxePinMode(LCD_DC, OUTPUT);
+      srxePinMode(LCD_RESET, OUTPUT);
       break;
     // CS (chip select) pin: Output level in arg_int
     case U8X8_MSG_GPIO_CS:
-      if (arg_int)
-        CS_PORT |= _BV(CS_BIT);
-      else
-        CS_PORT &= ~_BV(CS_BIT);
+      srxeDigitalWrite(LCD_CS, arg_int);
       break;
     // DC (data/cmd, A0, register select) pin: Output level in arg_int
     case U8X8_MSG_GPIO_DC:
-      if (arg_int)
-        DC_PORT |= _BV(DC_BIT);
-      else
-        DC_PORT &= ~_BV(DC_BIT);
+      srxeDigitalWrite(LCD_DC, arg_int);
       break;
     // Reset pin: Output level in arg_int
     case U8X8_MSG_GPIO_RESET:
-      if (arg_int)
-        RESET_PORT |= _BV(RESET_BIT);
-      else
-        RESET_PORT &= ~_BV(RESET_BIT);
+      srxeDigitalWrite(LCD_RESET, arg_int);
       break;
     default:
       u8x8_SetGPIOResult(u8x8, 1);
@@ -107,7 +157,7 @@ u8x8_gpio_and_delay (u8x8_t * u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 
 int main() {
     u8g2_t u8g2; // a structure which will contain all the data for one display
-    u8g2_Setup_st7586s_s028hn118a_2(&u8g2, U8G2_R0, u8x8_byte_4wire_sw_spi, u8x8_gpio_and_delay);  // init u8g2 structure
+    u8g2_Setup_st7586s_s028hn118a_2(&u8g2, U8G2_R0, u8x8_byte_avr_hw_spi, u8x8_gpio_and_delay);  // init u8g2 structure
     u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
     u8g2_SetPowerSave(&u8g2, 0); // wake up display
 
